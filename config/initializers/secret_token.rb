6ce8c6b852a9f5ac447e4ca4605f21092435f51a
Quentin So <quentin@primitus.com>
2011-04-19 17:14:47 +0800

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conimp::Application.config.secret_token = 'c0160c7276ed7d56390bf5936aa5ffb79919355b8595659eb885476e1d177475f7d8ba2dade5a10fdf57c47c20f9e642f3765b9f714c59f500f70cd579e82b72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Conimp::Application.config.session_store :cookie_store, :key => '_conimp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Conimp::Application.config.session_store :active_record_store
